51488f2a3938d05615ebb9ce9330ec58833a8daa
NsSuraj-01 <saisuraj1510@gmail.com>
2023-12-09 10:30:13 +0530

    Day 8 problems solved

--- Day_8.java
+++ Day_8.java
@@ -2,6 +2,33 @@ package Practise;
 
 public class Day_8 {
 
-    // triangular pattern
-    public static void printPattern
+    // triangular pattern with n rows
+    // time->O(n^2)
+    public static void triangularPattern(int n) {
+        for(int row=1; row<=n; row++) {
+            for(int i=1; i<=n-row; i++) {
+                System.out.print(" ");
+            }
+            for(int i=1; i<=2*(row)-1; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void leftpattern(int n) {
+        for(int row=1; row<=n; row++) {
+            for(int i=1; i<=n-row; i++) {
+                System.out.print(" ");
+            }
+            for(int i=1; i<=row; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        triangularPattern(5);
+    }
 }
